1bc85b8cf177dab74b23e67d4ad7c0eaa2fb4a55
Andhika Bagaskara Putra Nusantara <dikanusantara00@gmail.com>
2022-09-05 20:11:20 +0700

    second commit

---
+++ ABPN/src/main/java/latihan/AngkaRandom.java
@@ -0,0 +1,14 @@
+package latihan;
+import java.util.Random;
+
+public class AngkaRandom {
+	public static void main(String[] args) {
+		Random rand = new Random();
+		
+		for(int i=0; i<15; i++) {
+			if(rand.nextInt(5, 11) == 5) {
+				System.out.println("angka "+ 5 + " ketemu di index ke - " + i);
+			}
+		}
+	}
+}
---
+++ ABPN/src/main/java/latihan/Remedial.java
@@ -0,0 +1,33 @@
+package latihan;
+import java.util.*;
+
+public class Remedial {
+	int jumlah(int x, int y) {
+		return x+y;
+	}
+    public static void main(String[] args) {
+    	
+//    	ANALISA
+//    	INPUT BERUPA NUMERIC ATAU INTEGER
+//    	SETIAP SETELAH INPUT NUMERIC, AKAN LANGSUNG DIJUMLAHKAN DI SUM
+//    	JIKA SEKALI KITA INPUT SELAIN NUMERIC(HURUF ATAU SIMBOL) MAKA PROSES INPUT AKAN SELESAI
+//    	SETELAH INPUT SELESAI(ERROR) AKAN DITANGKAP OLEH CATCH DAN AKAN PRINT HASIL SUM DAN AKAN DI BREAK
+//    	SETELAH BREAK, PROSES AKAN LANGSUNG SELESAI KARENA TIDAK ADA NEXT STATEMENT
+    	
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int num = 0;
+
+        System.out.println("Masukkan angka sepuasnya. Jika sudah puas masukkan karakter: ");
+
+        do {
+            try {
+                num = sc.nextInt();
+                sum += num;
+            } catch (Exception e) {
+                System.out.println("Jumlah: "+sum);
+                break;
+            }
+        } while (true);
+    }
+}
