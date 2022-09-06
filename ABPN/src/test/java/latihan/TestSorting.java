3169d91276bb768b3553d1d252eb9de60b971966
Andhika Bagaskara Putra Nusantara <dikanusantara00@gmail.com>
2022-09-06 13:49:40 +0700

    commit

---
+++ ABPN/src/test/java/latihan/TestSorting.java
@@ -0,0 +1,44 @@
+package latihan;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.AssertJUnit;
+import static org.testng.Assert.*;
+import org.testng.annotations.*;
+import java.util.*;
+
+public class TestSorting {
+	Sorting st = new Sorting();
+	Random rand = new Random();
+	int[] intArr;
+	int indexAwal;
+	int indexAkhir;
+	int jumlahData;
+	
+	@BeforeClass
+	public void beforeClass() {
+		jumlahData = rand.nextInt(10, 20);
+		intArr = new int[jumlahData];
+		System.out.println("**TES SORTING DIMULAI**");
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		System.out.println("**TES SORTING SELESAI** \n");
+	}
+	
+	@Test
+	public void testSatu() {
+		do {
+			indexAwal = rand.nextInt(0, 100);
+			indexAkhir = rand.nextInt(0, 100);
+		}while(indexAkhir<indexAwal);
+		for(int i=0; i<jumlahData; i++) {
+			intArr[i] = rand.nextInt(indexAwal, indexAkhir);
+		}
+		System.out.println("====================================================");
+		System.out.println("TEST SELECTION SORT AND BUBBLE SORT");
+		AssertJUnit.assertEquals(st.selectionSortASCInt(intArr), st.bubbleSortASCInt(intArr));
+		System.out.println("====================================================");
+	}
+}
